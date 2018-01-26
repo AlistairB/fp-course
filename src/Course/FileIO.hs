2957e474e59573816027210023ecb14147ee1491
Alistair Burrowes <alistair.burrowes@gmail.com>
2018-01-26 15:01:24 +1100

    Completed FileIO

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -58,7 +58,7 @@ To test this module, load ghci in the root of the project directory, and do
 Example output:
 
 $ ghci
-GHCi, version ... 
+GHCi, version ...
 Loading package...
 Loading ...
 [ 1 of 28] Compiling (etc...
@@ -79,40 +79,46 @@ the contents of c
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
-main =
-  error "todo: Course.FileIO#main"
+main = do
+  filePaths <- getArgs
+  case filePaths of
+    (x :. _) -> run x
+    Nil -> pure ()
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@ and @printFiles@.
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run filePath = do
+  filesPointers <- lines <$> readFile filePath
+  files <- getFiles filesPointers
+  printFiles files
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles filePaths = sequence $ getFile <$> filePaths
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile filePath = do
+  contents <- readFile filePath
+  pure (filePath, contents)
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles files = do
+  sequence $ (\(fp, c) -> printFile fp c) <$> files
+  pure ()
 
 -- Given the file name, and file contents, print them.
 -- Use @putStrLn@.
@@ -120,5 +126,6 @@ printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile filePath contents = do
+  putStrLn $ "Printing for file: " ++ filePath
+  putStrLn contents
